19b2ab03d9f621e965e2c49159273a68cbedc547
LePhuocViet <no04022003name@gmail.com>
2023-07-11 20:35:13 +0700

    New Update

--- src/src/QuanLy.java
+++ src/src/QuanLy.java
@@ -2,26 +2,26 @@ package src;
 
 import java.util.Scanner;
 public class QuanLy extends NguoiList{
-
-    public static void main(String[] args){
-    int n;
     Scanner sc = new Scanner(System.in);
     NguoiList Arraynguoi = new NguoiList();
+    public void start(){
+    Arraynguoi.readflie(); 
     System.out.println("-------------------------------------------------");
     System.out.println("1.Delete Human");
     System.out.println("2.View Human");
     System.out.println("3.View Student");
     System.out.println("4.View Teacher");
-    Arraynguoi.readflie(); 
+    System.out.println("5.Add Human");
+    
+    int n;
+    Scanner sc = new Scanner(System.in);
+    NguoiList Arraynguoi = new NguoiList();
     do{
         System.out.println("Choose Number");
         n = sc.nextInt();
     }while(n > 4);
     switch(n){
-        case 1:
-        System.out.println("Nhap ma can xoa");
-        int d = sc.nextInt();
-        Arraynguoi.xoanguoi(d);
+        case 1: deleted();
         case 2:
         System.out.println(Arraynguoi.viewnguoi());
         break;
@@ -33,8 +33,17 @@ public class QuanLy extends NguoiList{
         break;
         case 5:
         break;
+        case 6:
+        break;
         
     }
-
+    }
+    public void deleted(){
+     System.out.println("Nhap ma can xoa");
+        int d = sc.nextInt();
+        Arraynguoi.xoanguoi(d); 
 }
+    public static void main(String[] args){
+        QuanLy quanLy = new QuanLy();
+        quanLy.run();
 }
